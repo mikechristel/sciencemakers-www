d2bb738964b6b55eaade98594f8a9db170ac104d
Mike Christel <christel@cmu.edu>
2020-12-15 11:55:50 -0500

    cached changes from BEFORE API update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   production: true,
   hmr: false,
   // Runtime configuruation
-  serviceBase: "https://da-production-api.azurewebsites.net/api/",
+  serviceBase: "https://sm-production-api.azurewebsites.net/api/", // was: "https://da-production-api.azurewebsites.net/api/",
   mediaBase: "https://daproductionstorage.blob.core.windows.net/media/",
   firstInterviewYear: 1993
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   hmr: false,
   // Runtime configuruation
-  serviceBase: "https://da-production-api.azurewebsites.net/api/",
+  serviceBase: "https://sm-production-api.azurewebsites.net/api/", // was: "https://da-production-api.azurewebsites.net/api/",
   mediaBase: "https://daproductionstorage.blob.core.windows.net/media/",
   firstInterviewYear: 1993
 };
