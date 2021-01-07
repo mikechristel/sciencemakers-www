2dd6c33350f5e70911ac1272b0deab842d2b0aff
Mike Christel <christel@cmu.edu>
2021-01-07 10:21:17 -0500

    fixed padding on list stamps for readability

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -57,7 +57,7 @@ export class HomeComponent extends BaseComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.titleManagerService.setTitle("ScienceMakers Digital Archive (January 6, 2021)");
+        this.titleManagerService.setTitle("ScienceMakers Digital Archive (January 7, 2021)");
         this.liveAnnouncer.announce("ScienceMakers Digital Archive"); // NOTE: using LiveAnnouncer to eliminate possible double-speak
 
         this.historyMakerService.getCorpusSpecifics().pipe(takeUntil(this.ngUnsubscribe))
