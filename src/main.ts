ed541004fc46bf3a260e3d417f24b2aabee4cf27
mikechristel <christel@cmu.edu>
2026-04-03 12:45:34 -0400

    Removed zone and old unused karma/jasmine testing

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import 'zone.js'; // Included with Angular CLI
 import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { provideRouter } from '@angular/router';
